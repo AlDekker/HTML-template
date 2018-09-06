b3c0efb2be380c07911ea1c06b7af53ff593ca57
Dekker <alexander-dekker@yandex.ru>
2018-09-06 17:16:51 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Starting-HTML-template
+# Starting HTML-template
 Template for quick started projects.
+
+### Include:
+  + Gulp;
+  + Bower.
+
+### Libs:
++ bootstrap v.4;
++ jQuery;
++ fontawesome;
++ html5shiv;
++ Modernizr.
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    browserSync   = require('browser-sync'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglify'),
+    minCss        = require('gulp-clean-css'),
+    clean         = require('gulp-clean'),
+    rename        = require('gulp-rename');
+
+
+gulp.task('sass', function(){
+  return gulp.src('app/sass/**/*.sass')
+  .pipe(sass({
+    outputStyle: 'expanded',
+    includePaths: require('bourbon').includePaths
+  }).on('error', sass.logError))
+  .pipe(autoprefixer({
+   browsers: ['last 15 versions'],
+   cascade: false
+ }))
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({stream: true}))
+});
+
+
+gulp.task('browserSync' , function(){
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+    // tunnel: true,
+    // tunnel: "projectname"
+	});
+});
+
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
+
+
+/* for building Project */
+
+gulp.task('minCSS', function(){
+  return gulp.src('app/css/style.css')
+  .pipe(minCss())
+  .pipe(rename({
+    // prefix: "min-",
+    suffix: "-min",
+    // extname: ".css"
+  }))
+  .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('minJS', function(){
+  return gulp.src('app/js/common.js')
+  .pipe(uglify())
+  .pipe(rename({
+    // prefix: "min-",
+    suffix: "-min",
+    // extname: ".js"
+  }))
+  .pipe(gulp.dest('dist/js'))
+});
+
+
+gulp.task('build', ['sass', 'minCSS', 'minJS'], function(){
+
+  /*After build сhange the paths to the minified files in index.html*/
+
+  // Some files
+  gulp.src([
+    'app/*.html',
+    'app/htacce.ss',
+    'app/robots.txt'
+    ]).pipe(gulp.dest('dist'));
+
+  // JS
+  gulp.src([
+    'app/js/**/*.js',
+    ]).pipe(gulp.dest('dist/js'));
+
+  // Styles
+  gulp.src([
+    'app/css/**/*.css',
+    ]).pipe(gulp.dest('dist/css'));
+
+  // Fonts
+  gulp.src([
+    'app/fonts/**/*',
+    ]).pipe(gulp.dest('dist/fonts'));
+
+  // Libs
+  gulp.src([
+    'app/libs/**/*',
+    ]).pipe(gulp.dest('dist/libs'));
+
+  // Images
+    gulp.src([
+    'app/img/**/*',
+    ]).pipe(gulp.dest('dist/img'));
+
+
+
+
+});
+
+gulp.task('cleanBuild', function () {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+
